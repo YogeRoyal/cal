9b5a49b51dffacf1ae2a95010c7678e31a48087a
unknown <kaketiyogesh2110.com>
2023-11-17 10:29:43 +0530

    first commit

---
+++ src/calculator.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input first number
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        // Input operator
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        // Input second number
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Perform calculation based on operator
+        double result = 0;
+
+        if (operator == '+')
+        {
+            result = num1 + num2;
+        }
+         else if (operator == '-')
+         {
+             result = num1 - num2;
+         }
+         else if (operator == '*')
+         {
+             result = num1 * num2;
+        }
+         else if (operator == '/')
+         {
+              if (num2 != 0)
+              {
+                result = num1 / num2;
+        }
+              else
+              {
+            System.out.println("Error: Cannot divide by zero");
+            // Exit the program
+            System.exit(1);
+        }
+    }
+         else
+         {
+        System.out.println("Error: Invalid operator");
+        // Exit the program
+        System.exit(1);
+    }
+
+    // Display the result
+        System.out.println("Result: " + result);
+
+    // Close the scanner
+        scanner.close();
+}
+
+    }
+
